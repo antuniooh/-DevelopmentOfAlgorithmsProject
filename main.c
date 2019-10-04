027f16675e925f550d6001e5f6e242edb9cdffb4
antuniooh <51217271+antuniooh@users.noreply.github.com>
2019-10-04 17:47:17 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -21,71 +21,8 @@ struct Data criadata(int dia, int mes, int ano){
 struct Receita{
     struct Data data;
     float valor;
-    char descricao[200];
+    char categoria[200];
 };
-
-struct Livro{
-    struct Receita receita[100];
-    int n;
-};
-
-void tiraEnter(char *s){
-    for (int i = 0; s[i] != '\n' ; ++i) {
-        s[i] = 0;
-    }
-}
-
-struct Receita perguntaUsuarioReceita(){
-    struct Receita r;
-
-    puts("Entre com os dados da receita: ");
-    puts("Valor: ");
-    scanf("%f", &(r.valor));
-    puts("Data: ");
-    scanf("%d %d %d", &(r.data.dia),&(r.data.mes), &(r.data.ano));
-    fflush(stdin);//limpa o enter do buffer que foi deixado
-    puts("Cliente: ");
-    fgets(r.cliente, 200, stdin);
-    tiraEnter(r.cliente);
-    puts("Obrigado!");
-
-    return r;
-}
-
-void cadastraReceita(struct Livro* l){
-    struct Receita nova = perguntaUsuarioReceita();
-    (*l).receita[(*l).n] = nova;
-    (*l).n++;
-    //l->receita == (*l).receita
-}
-
-void imprimeReceita(struct Receita r){
-    puts("Imprimindo receita");
-    printf("Nome: %s\n", r.cliente); //vem com o enter do fgets, possivel percorrer a string e tirar o \n
-    printf("Valor: %f\n", r.valor);
-    printf("Data: %d/%d/%d\n", r.data);
-
-}
-
-void imprimeLivro(struct Livro l) {
-    int i;
-    for (int i = 0; i < l.n; i++) {
-        imprimeReceita(l.receita[i]);
-        puts("--------------------");
-    }
-}
-
-void removeReceita(struct Livro* l){
-    puts("Qual posicao");
-    int pos;
-    scanf("%d", &pos);
-    pos--;
-    int i;
-    for (i = pos; i < l->n - 1; ++i) {
-        l->receita[i] =  l->receita[i+1];
-    }
-    l->n--;
-}
 
 
 int menu(){
@@ -103,22 +40,80 @@ int menu(){
     return operacao;
 }
 
+void tiraEnter(char *s){
+    for (int i = 0; s[i] != '\n' ; ++i) {
+        s[i] = 0;
+    }
+}
+
+void Inserir(FILE *p){
+    struct Receita r;
+
+    int op;
+    puts("Voce deseja inserir uma receita ou um gasto?");
+    puts("1 - Receita");
+    puts("2 - Gasto");
+    printf("Opcao > ");
+    scanf(" %d",&op);
+
+    puts("Qual o valor? ");
+    scanf(" %f",&(r.valor));
+    puts("Qual a data? ");
+    scanf("%d %d %d", &(r.data.dia), &(r.data.mes),&(r.data.ano));
+    puts("Qual a categoria?: (Moradia, Estudos, Transporte, Alimentacao, Trabalho)");
+    fflush(stdin);//limpa o enter do buffer que foi deixado
+    fgets(r.categoria, 200, stdin);
+    //tiraEnter(r.categoria);
+
+    if(op == 1){
+        fprintf(p, "%s", "Data: ");
+        fprintf(p, " %d/%d/%d ", r.data.dia, r.data.mes, r.data.ano);
+        fprintf(p, "%s", " + ");
+        fprintf(p, " %f ", r.valor);
+        fprintf(p, "%s\n", r.categoria);
+    } else if(op == 2){
+        fprintf(p, "%s", "Data: ");
+        fprintf(p, " %d/%d/%d ", r.data.dia, r.data.mes, r.data.ano);
+        fprintf(p, "%s", " - ");
+        fprintf(p, " %f ", r.valor);
+        fprintf(p, " %s \n", r.categoria);
+    }
+}
+
 int main() {
-    struct Livro l;
-    l.n = 0;
+    // criando a variável ponteiro para o arquivo
+    FILE *pont_arq;
+    char texto[2000];
+
+    //abrindo o arquivo
+    pont_arq = fopen("financas.txt", "a");
+
+    //usando fprintf para armazenar a string no arquivo
+    //fprintf(pont_arq, "%s", "Minhas Finanças Pessoais\n");
+    //fprintf(pont_arq, "%s", "==============================================\n");
+
+
+    // fechando arquivo
+
+
 
     while (1){
         int op = menu();
         if (op == 1){
-            //Inserir();
+            Inserir(pont_arq);
         }
         else if (op == 2){
            // Remover();
         }
         else if (op == 3){
+            //e será impresso o texto
+            while(fgets(texto, 2000, pont_arq) != NULL)
+                printf("%s", texto);
+
         }
         else if (op == 0){
             puts("Fechando o sistema....");
+            fclose(pont_arq);
             break;
         }
         else {
