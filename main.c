cecf78af71871e3f331807bab922be89527a607d
antuniooh <51217271+antuniooh@users.noreply.github.com>
2019-10-08 16:37:24 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 struct Data{
     int dia;
     int mes;
@@ -24,6 +25,12 @@ struct Receita{
     char categoria[200];
 };
 
+struct Usuario{
+    char nome[200];
+    int CPF ;
+    float saldo;
+    char senha[4];
+};
 
 int menu(){
     puts("=================");
@@ -46,79 +53,129 @@ void tiraEnter(char *s){
     }
 }
 
-void Inserir(FILE *p){
+void Inserir(FILE *p, FILE* p_user, struct Usuario u){
     struct Receita r;
 
-    int op;
-    puts("Voce deseja inserir uma receita ou um gasto?");
-    puts("1 - Receita");
-    puts("2 - Gasto");
-    printf("Opcao > ");
-    scanf(" %d",&op);
+    void criaReceita(){
 
-    puts("Qual o valor? ");
-    scanf(" %f",&(r.valor));
-    puts("Qual a data? ");
-    scanf("%d %d %d", &(r.data.dia), &(r.data.mes),&(r.data.ano));
-    puts("Qual a categoria?: (Moradia, Estudos, Transporte, Alimentacao, Trabalho)");
-    fflush(stdin);//limpa o enter do buffer que foi deixado
-    fgets(r.categoria, 200, stdin);
-    //tiraEnter(r.categoria);
+        int op;
+        puts("Voce deseja inserir uma receita ou um gasto?");
+        puts("1 - Receita");
+        puts("2 - Gasto");
+        printf("Opcao > ");
+        scanf(" %d",&op);
 
-    if(op == 1){
-        fprintf(p, "%s", "Data: ");
-        fprintf(p, " %d/%d/%d ", r.data.dia, r.data.mes, r.data.ano);
-        fprintf(p, "%s", " + ");
-        fprintf(p, " %f ", r.valor);
-        fprintf(p, "%s\n", r.categoria);
-    } else if(op == 2){
-        fprintf(p, "%s", "Data: ");
-        fprintf(p, " %d/%d/%d ", r.data.dia, r.data.mes, r.data.ano);
-        fprintf(p, "%s", " - ");
-        fprintf(p, " %f ", r.valor);
-        fprintf(p, " %s \n", r.categoria);
+        puts("Qual o valor? ");
+        scanf(" %f",&(r.valor));
+        puts("Qual a data? ");
+        scanf("%d %d %d", &(r.data.dia), &(r.data.mes),&(r.data.ano));
+        puts("Qual a categoria?: (Moradia, Estudos, Transporte, Alimentacao, Trabalho)");
+        fflush(stdin);//limpa o enter do buffer que foi deixado
+        fgets(r.categoria, 200, stdin);
+        //tiraEnter(r.categoria);
+
+        if(op == 1){
+            fprintf(p, "%s", "Data: ");
+            fprintf(p, " %d/%d/%d ", r.data.dia, r.data.mes, r.data.ano);
+            fprintf(p, "%s", " + ");
+            fprintf(p, " %f ", r.valor);
+            fprintf(p, "%s\n", r.categoria);
+        } else if(op == 2){
+            fprintf(p, "%s", "Data: ");
+            fprintf(p, " %d/%d/%d ", r.data.dia, r.data.mes, r.data.ano);
+            fprintf(p, "%s", " - ");
+            fprintf(p, " %f ", r.valor);
+            fprintf(p, " %s \n", r.categoria);
+        }
     }
+
+    char senha[4];
+    puts("Necessario a senha para fazer movimentacoes! ");
+    fflush(stdin);//limpa o enter do buffer que foi deixado
+    fgets(senha, 4, stdin);
+
+    printf("A senha: %s\n", u.senha);
+    printf("A senha: %s\n", senha);
+
+    if(senha ==  (u.senha)) {
+        printf("senhas coincidem\n");
+        //criaReceita();
+
+    } else{
+        printf("n senhas coincidem\n");
+    }
+
+}
+
+void criaUsuario(FILE *p, struct Usuario* u){
+
+    puts("=================");
+    puts("Criacao de Conta");
+    puts("=================");
+    puts("Qual o seu nome? ");
+    fflush(stdin);//limpa o enter do buffer que foi deixado
+    fgets(u->nome, 200, stdin);
+    puts("Qual seu CPF? ");
+    scanf("%d", u->CPF);
+    fflush(stdin);//limpa o enter do buffer que foi deixado
+    u->saldo = 0.0;
+    puts("Qual a sua senha? (deve ter 4 digitos");
+    fflush(stdin);//limpa o enter do buffer que foi deixado
+    fgets(u->senha, 4, stdin);
+    fflush(stdin);//limpa o enter do buffer que foi deixado
+
+
+    //adiciona no arquivo
+    fprintf(p, " %s", u->nome);
+    fprintf(p, "%d\n", u->CPF);
+    fprintf(p, " %f\n", u->saldo);
+    fprintf(p, "%s\n", u->senha);
+
+    //fecha o arquiv
+    fclose(p);
+
+
 }
 
 int main() {
-    // criando a variável ponteiro para o arquivo
-    FILE *pont_arq;
-    char texto[2000];
+    //iniciar um usuário
+    struct Usuario u;
+    FILE *p_user = fopen("user.txt", "r");;
 
-    //abrindo o arquivo
-    pont_arq = fopen("financas.txt", "a");
+    if(p_user == NULL)
+    {
+        p_user = fopen("user.txt", "a");
+        criaUsuario(p_user, &(u));
+        main();
+    } else {
+        // criando a variável ponteiro para o arquivo
+        FILE *pont_arq;
 
-    //usando fprintf para armazenar a string no arquivo
-    //fprintf(pont_arq, "%s", "Minhas Finanças Pessoais\n");
-    //fprintf(pont_arq, "%s", "==============================================\n");
+        //abrindo o arquivo
+        pont_arq = fopen("financas.txt", "a");
 
+        while (1) {
+            int op = menu();
+            if (op == 1) {
+                printf("%s\n", u.nome);
+                printf("%f\n", u.saldo);
+                printf("%s\n", u.senha);
+                printf("%d\n", u.CPF);
 
-    // fechando arquivo
+                Inserir(pont_arq, p_user, u);
+            } else if (op == 2) {
+                // Remover();
+            } else if (op == 3) {
 
-
-
-    while (1){
-        int op = menu();
-        if (op == 1){
-            Inserir(pont_arq);
-        }
-        else if (op == 2){
-           // Remover();
-        }
-        else if (op == 3){
-            //e será impresso o texto
-            while(fgets(texto, 2000, pont_arq) != NULL)
-                printf("%s", texto);
-
-        }
-        else if (op == 0){
-            puts("Fechando o sistema....");
-            fclose(pont_arq);
-            break;
-        }
-        else {
-            puts("Operação Inválida, fechando o sistema....");
-            break;
+            } else if (op == 0) {
+                puts("Fechando o sistema....");
+                fclose(pont_arq);
+                break;
+            } else {
+                puts("Operação Inválida, fechando o sistema....");
+                fclose(pont_arq);
+                break;
+            }
         }
     }
 
