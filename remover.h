56fc82a576bfe30f76fdd4ccd8a324feb83625bc
antuniooh <51217271+antuniooh@users.noreply.github.com>
2019-11-04 20:59:56 -0300

    Create remover.h

---
+++ remover.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#ifndef REMOVER_H_
+#define REMOVER_H_
+
+void Remover(FILE *MyPersonalWallet, FILE *user);
+void ApagaLinha(FILE *MyPersonalWallet, FILE *user, const int num);
+void RemoverTudo();
+
+#endif
+
