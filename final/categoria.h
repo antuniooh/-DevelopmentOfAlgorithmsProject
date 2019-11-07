4eb563f9b3d8ea84a167ebb6c39f3571bf404227
henriquevital00 <henriquevital1000@hotmail.com>
2019-11-06 22:55:56 -0200

    Apagando a pasta final

--- final/categoria.h
+++
@@ -1,8 +0,0 @@
-#ifndef CATEGORIA_H_
-#define CATEGORIA_H_
-
-void ImprimeCategoria(FILE *file, char* categoria);
-void Anual(FILE *MyPersonalWallet, char *ano);
-void Imprime(FILE *file);
-
-#endif
