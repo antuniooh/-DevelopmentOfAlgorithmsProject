309e884fb4b813cae0e24dc20fe074b885365980
henriquevital00 <48650626+henriquevital00@users.noreply.github.com>
2019-11-06 21:13:58 -0300

    Delete categoria.h

--- categoria.h
+++
@@ -1,9 +0,0 @@
-#ifndef CATEGORIA_H_
-#define CATEGORIA_H_
-
-void ImprimeCategoria(FILE *file, char* categoria);
-void Anual(FILE *MyPersonalWallet, char *ano);
-void ImprimeCategoriaMes(FILE *file);
-void Imprime(FILE *file);
-
-#endif
